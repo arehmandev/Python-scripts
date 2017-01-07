c38eaa88757adff77abee2eb416c348b3be071c9
arehmandev <arehmandev@outlook.com>
2017-01-07 01:17:49 +0000

    added fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
-count = 0
-if count == 0:
-    print "0 is undivisable"
-elif count % 3 == 0:
+import sys
+count = sys.argv[1]
+
+if int(count) % 5 == 0 and int(count) % 3 == 0:
+    print "FizzBuzz"
+elif int(count) % 3 == 0:
     print "Fizz"
-else:
+elif int(count) % 5 == 0:
     print "Buzz"
+else:
+    print "Not a multiple of 5 or 3"
