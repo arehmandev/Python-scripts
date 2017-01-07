82052a840f79d4fd029aa8a9aa1011b97b73094d
arehmandev <arehmandev@outlook.com>
2017-01-07 01:21:47 +0000

    Added number options to fizz buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,14 @@
 import sys
 count = sys.argv[1]
 
-if int(count) % 5 == 0 and int(count) % 3 == 0:
+divider1 = 3
+divider2 = 5
+
+if int(count) % int(divider1) == 0 and int(count) % int(divider2) == 0:
     print "FizzBuzz"
-elif int(count) % 3 == 0:
+elif int(count) % int(divider1) == 0:
     print "Fizz"
-elif int(count) % 5 == 0:
+elif int(count) % int(divider2) == 0:
     print "Buzz"
 else:
-    print "Not a multiple of 5 or 3"
+    print "Not a multiple of", int(divider1), "or", int(divider2)
